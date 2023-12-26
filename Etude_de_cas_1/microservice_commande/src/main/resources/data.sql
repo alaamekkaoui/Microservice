abc1133639b1afdd237000c5636d9700a0378022
DsDemon <dsdemon06@gmail.com>
2023-12-26 22:52:57 +0100

    .

--- Etude_de_cas_1/microservice_commande/src/main/resources/data.sql
+++ Etude_de_cas_1/microservice_commande/src/main/resources/data.sql
@@ -2,7 +2,7 @@ INSERT INTO commande (id, description, date, quantite, montant)
 VALUES (1, 'Description 1', '2023-12-21', 10, 100.50);
 
 INSERT INTO commande (id,description, date, quantite, montant)
-VALUES (2,'Description 2', '2023-12-20', 5, 50.75);
+VALUES (2,'Description 2', '2023-12-10', 5, 50.75);
 
 INSERT INTO commande (id,description, date, quantite, montant)
 VALUES (3,'Description 3', '2023-03-07', 8, 80.00);
